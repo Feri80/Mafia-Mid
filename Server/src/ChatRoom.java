05eece8c9a4adcf4c91b12f095c4cef53fdb71c5
Feri80 <bigfarhad7780@gmail.com>
2021-05-24 22:47:57 +0430

    chatroom added

--- Server/src/ChatRoom.java
+++ Server/src/ChatRoom.java
@@ -1,14 +1,28 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class ChatRoom 
 {
 
-    
+    private ArrayList<Player> players;
+
+    public ChatRoom(ArrayList<Player> players)
+    {
+        this.players = players;
+    }
+
+
+    public void sendTo(Chat chat, Player dest) 
+    {
+        
+    }
+
     public void sendTo(Chat chat, ArrayList<Player> dest)
     {
 
     }
 
-
+    public void sendToAll(Chat chat)
+    {
+        sendTo(chat, players);
+    }
 }
